1520b031d2a158f5f155930bbc209e666a4cefcb
ashurohom <ashitosh.rohom@gmail.com>
2025-07-08 10:57:06 +0530

    Initial Commit

---
+++ blogplatform/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post
+from .serializers import PostSerializers
+# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializers
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
