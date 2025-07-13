2f47acae6045785d690e846e26968196d9ec5fb5
ashurohom <ashitosh.rohom@gmail.com>
2025-07-13 16:30:17 +0530

    Filtering and Search in Blog API

--- blogplatform/blog/views.py
+++ blogplatform/blog/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Post
 from .serializers import PostSerializers
+from rest_framework import filters  
 # Create your views here.
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset=Post.objects.all().order_by('-created_at')
     serializer_class=PostSerializers
     permission_classes=[permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title','content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
     def get_queryset(self):
-        return Post.object.filter(author=self.request.user)    
+        return Post.objects.filter(author=self.request.user)    
